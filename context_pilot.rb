1c4fa9cb0ecb15aebd888523f33cd238684e35a2
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-05-11 16:44:49 +0530

    homebrew formula for context_pilot

---
+++ context_pilot.rb
@@ -0,0 +1,18 @@
+class ContextPilot < Formula
+  desc "Get Context at your code cursor!"
+  homepage "https://github.com/krshrimali/context-pilot-rs"
+  url "https://github.com/krshrimali/context-pilot-rs/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "72c3a69a5ae256cffcc92ba124a1c013473af3ac0d764ca3ee7943223cf1f439"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    system "cp", "target/release/context_pilot", "#{bin}/context_pilot"
+  end
+
+  test do
+    system "#{bin}/context_pilot", "--help"
+  end
+end
